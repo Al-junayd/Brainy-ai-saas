95957cd6c86dfd6ef4fb37e3f8b4b866b697221a
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2023-08-23 15:33:30 +0100

    completed dashboard page

---
+++ app/(dashboard)/(routes)/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "@/components/ui/Navbar";
+import Sidebar from "./Sidebar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full relative">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:inset-y-0 md:fixed  z-[80] bg-gray-900">
+        {" "}
+        <Sidebar />
+      </div>
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
